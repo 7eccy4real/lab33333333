7fc675913c7b8819b4fe2339b4d1db2e81b5e246
Arthur Gabbasov <jecxylay@bk.ru>
2024-04-09 17:08:37 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+#include "Header1.h"
+
+int main() {
+	Square sq(3.0);
+	sq.CalculateArea();
+	sq.ShowInfo();
+
+	Triangle tr(2, 5);
+	tr.CalculateArea();
+	tr.ShowInfo();
+
+	Circle cr(5);
+	cr.CalculateArea();
+	cr.ShowInfo();
+
+	TriangularPyramid pyramid(6, 5, 4);
+	pyramid.CalculateVolume();
+	pyramid.ShowInfo();
+
+	Cylinder cyl(7, 2);
+	cyl.CalculateVolume();
+	cyl.ShowInfo();
+
+
+	Sphere sp(4);
+	sp.CalculateVolume();
+	sp.ShowInfo();
+
+
+	return 0;
+}
